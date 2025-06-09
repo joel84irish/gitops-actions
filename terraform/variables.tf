72529418f5f7e596a8d2e7211f8385a4a53fe076
joel84irish <joelirish@hotmail.co.uk>
2025-06-09 21:01:50 +0100

    updated variables.tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,7 +26,7 @@ variable "public_subnet_cidr_blocks" {
   default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
-variable "tags" {
+variable "tags" { #cluster name
   default = {
     App = "eks-cluster"
   }
