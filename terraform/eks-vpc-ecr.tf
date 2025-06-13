bff9ab86e6852573f727d8a6715bab8179ad6532
joel84irish <joelirish@hotmail.co.uk>
2025-06-13 20:43:51 +0100

    updated module version fmt

--- terraform/eks-vpc-ecr.tf
+++ terraform/eks-vpc-ecr.tf
@@ -31,9 +31,9 @@ module "eks" {
   source  = "github.com/terraform-aws-modules/terraform-aws-eks//?ref=d2b1f3b"
   version = "~> 20.31"
 
-  cluster_name                    = var.name
-  cluster_version                 = var.k8s_version
-  cluster_endpoint_public_access  = true
+  cluster_name                   = var.name
+  cluster_version                = var.k8s_version
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
